e9aaa370fa484c73ea1125e24f6e14f6d176c688
Armada <73616858+Armada3334@users.noreply.github.com>
2023-01-19 22:46:20 -0500

    messed up the commit

--- testing/images.py
+++ testing/images.py
@@ -11,7 +11,7 @@ with open('client_id.txt', 'r') as f: # Read the client ID from a file. You'll h
 RPC = Presence(client_id=client_id)
 RPC.connect()
 
-# Make sure you are using the same name that you used when uploading the image
+# Make sure you are using the same name that you used when uploading the image, then you can set an alternate option for the text that shows when you hover over the image.
 RPC.update(large_image="hungy", large_text="HUMBGHRY",
             small_image="small-image", small_text="Small Text Here!")
 
